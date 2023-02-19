c7a1181a18460dad4db30c2a423dcc726d98039e
Mengde Yang <yang.mengde@northeastern.edu>
2023-02-18 21:35:03 -0500

    Parallel Sorting

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -16,14 +16,15 @@ import java.util.concurrent.ForkJoinPool;
  */
 public class Main {
 
+    public static ForkJoinPool myPool = new ForkJoinPool(2);
     public static void main(String[] args) {
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+        System.out.println("Degree of parallelism: " + myPool.getParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+        int[] array = new int[16000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
+        for (int j = 0; j < 20; j++) {
+            ParSort.cutoff = 400000 * (j + 1);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
